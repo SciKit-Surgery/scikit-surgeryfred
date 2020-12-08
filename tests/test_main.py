37b2090b7960380b06fc76a0ff5a5b2e22b25155
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-12-08 13:25:36 +0000

    Issue #28 added example test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,24 @@
 # coding=utf-8
 
 """Fiducial Registration Educational Demonstration tests"""
-
+import pytest
 import main as sksfmain # pylint: disable=unused-import
 
 # Pytest style
+
+# pylint: disable=redefined-outer-name
+@pytest.fixture
+def client():
+    """
+    A fixture to use Flask's testing skeleton
+    https://flask.palletsprojects.com/en/1.1.x/testing/
+    """
+    with sksfmain.app.test_client() as myclient:
+        yield myclient
+
+
+def testserveindex(client):
+    """Serve up the index page"""
+
+    _index = client.get('/')
+    print (_index.data)
