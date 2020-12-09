062acac5fdd2df4e5ec233154a4fb4533bc22c89
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-12-09 10:09:10 +0000

    Issue #31 added failing tests for get method

--- tests/test_main.py
+++ tests/test_main.py
@@ -51,3 +51,67 @@ def testserveindex(client):
     parser = FredHTMLParser('405 Method Not Allowed')
     parser.feed(str(index.data))
     assert parser.title_ok
+
+def testserve_defaultcontour(client):
+    """Serve default contour"""
+    #get should not be allowed
+    contour = client.get('/defaultcontour')
+    parser = FredHTMLParser('405 Method Not Allowed')
+    parser.feed(str(contour.data))
+    assert parser.title_ok
+
+def testserve_gettarget(client):
+    """Serve target"""
+    #get should not be allowed
+    target = client.get('/gettarget')
+    parser = FredHTMLParser('405 Method Not Allowed')
+    parser.feed(str(target.data))
+    assert parser.title_ok
+
+def testserve_getfle(client):
+    """Serve fle"""
+    #get should not be allowed
+    fle = client.get('/getfle')
+    parser = FredHTMLParser('405 Method Not Allowed')
+    parser.feed(str(fle.data))
+    assert parser.title_ok
+
+def testserve_placefiducial(client):
+    """Serve place fiducial"""
+    #get should not be allowed
+    fid = client.get('/placefiducial')
+    parser = FredHTMLParser('405 Method Not Allowed')
+    parser.feed(str(fid.data))
+    assert parser.title_ok
+
+def testserve_register(client):
+    """Serve register"""
+    #get should not be allowed
+    reg = client.get('/register')
+    parser = FredHTMLParser('405 Method Not Allowed')
+    parser.feed(str(reg.data))
+    assert parser.title_ok
+
+def testserve_initdatabase(client):
+    """Serve init db"""
+    #get should not be allowed
+    database = client.get('/initdatabase')
+    parser = FredHTMLParser('405 Method Not Allowed')
+    parser.feed(str(database.data))
+    assert parser.title_ok
+
+def testserve_writeresults(client):
+    """Serve write results"""
+    #get should not be allowed
+    database = client.get('/writeresults')
+    parser = FredHTMLParser('405 Method Not Allowed')
+    parser.feed(str(database.data))
+    assert parser.title_ok
+
+def testserve_correlation(client):
+    """Serve default contour"""
+    #get should not be allowed
+    corr = client.get('/correlation')
+    parser = FredHTMLParser('405 Method Not Allowed')
+    parser.feed(str(corr.data))
+    assert parser.title_ok
