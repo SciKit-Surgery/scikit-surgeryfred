e0744b93d49f5273b6f379b5a474f5602e5b00b3
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-01-19 16:27:22 +0000

    #obsolete removed imports

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ import math
 import datetime
 # Flask
 from flask import Flask, request, render_template, jsonify
-import numpy as np
-from google.cloud import firestore
-from google.auth.exceptions import DefaultCredentialsError
-from sksurgeryfred.algorithms.point_based_reg import PointBasedRegistration
-from sksurgeryfred.algorithms.fred import make_target_point, is_valid_fiducial
-from sksurgeryfred.algorithms.errors import expected_absolute_value
-from sksurgeryfred.algorithms.fle import FLE
-from sksurgeryfred.algorithms.scores import calculate_score
-from sksurgeryfred.utilities.results_database import ResultsDatabase
-from sksurgeryfred import __version__ as fredversion
 
 # Declare a flask app
 app = Flask(__name__)
