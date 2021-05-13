ced13be913390fdba0efee87ddb7c5192d56a99f
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-05-13 15:26:03 +0100

    Issue #62 added script to get results

--- sksurgeryfred/utilities/get_results.py
+++ sksurgeryfred/utilities/get_results.py
@@ -3,13 +3,32 @@ Script to download and parse results from firestore
 """
 
 from google.cloud import firestore
+from sksurgeryfred.utilities.results_database import ResultsDatabase
 
-database = firestore.Client()
+def get_results(testing = False):
+    """
+    Function to download results from our results database and
+    parse them ready for analysis
+    """
+    database = None
+    if testing:
+        database = ResultsDatabase('empty')
+    else:
+        database = firestore.Client()
 
-results=database.collection("results").get()
+    results=database.collection("results").get()
 
-for result in results:
-    print (result.to_dict())
-    game_results = result.reference.collection("game_results").get()
-    for game_result in game_results:
-        print (game_result.to_dict())
+    for result in results:
+        result_dictionary = result.to_dict()
+        time = result_dictionary.get('time')
+        version = result_dictionary.get('fred verion')
+        game_results = result.reference.collection("game_results").get() #pylint: disable=no-member
+        for game_result in game_results:
+
+            result_dictionary = game_result.to_dict()
+            category = result_dictionary.get('state')
+            score = result_dictionary.get('score')
+            margin = result_dictionary.get('margin')
+
+            print (time, ', fred version', version, ', ', category,
+                            ', ', score, ', ', margin)
