e939ee9cfe3186c13890d81972b9e235ea625a0b
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-05-18 14:39:30 +0100

    Issue #62 added get_results tests

---
+++ tests/utilities/test_get_results.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+"""Fiducial Registration Educational Demonstration tests"""
+from sksurgeryfred.utilities.get_results import get_results
+
+def test_getresults():
+    """
+    Test the GetResults function
+    """
+    get_results(testing = True)
